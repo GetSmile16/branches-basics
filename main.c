2074ec68c7591b3a31b638396274ee0333565594
Vladislav <ivlad160101@gmail.com>
2020-02-26 18:03:25 +0700

    Add Dev function

--- main.c
+++ main.c
@@ -5,11 +5,17 @@ int minus(int a, int b)
     return a - b;
 }
 
+int dev(int a, int b)
+{
+    return a / b;
+}
+
 int main()
 {
     int n = 8;
     int m = 2;
     printf("Hello, World!");
     printf("%d - %d = %d", n, m, minus(n, m));
+    printf("%d / %d = %d", n, m, dev(n, m));
     return 0;
 }
