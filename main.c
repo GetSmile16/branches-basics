daa87e09953852bbe5c9473136189964b87377e8
Vladislav <ivlad160101@gmail.com>
2020-02-26 17:41:43 +0700

    Add Mult function

--- main.c
+++ main.c
@@ -5,6 +5,11 @@ int summ(int a, int b)
     return a + b;
 }
 
+int mult(int a, int b)
+{
+    return a * b
+}
+
 int main()
 {
     int n = 8;
@@ -12,6 +17,7 @@ int main()
 
     printf("Hello, World!");
     printf("%d + %d = %d", n, m, summ(n, m));
+    printf("%d * %d = %d", n, m, mult(n, m));
 
     return 0;
 }
