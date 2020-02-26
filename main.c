e808da72069199bf27680a2604154eeb1df6c60e
Vladislav <ivlad160101@gmail.com>
2020-02-26 18:01:22 +0700

    Add Minus function

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int minus(int a, int b)
+{
+    return a - b;
+}
+
 int main()
 {
+    int n = 8;
+    int m = 2;
     printf("Hello, World!");
+    printf("%d - %d = %d", n, m, minus(n, m));
     return 0;
 }
