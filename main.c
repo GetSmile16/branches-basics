6f8aad903ae6102b910c1c5998a792f59d642573
Vladislav <ivlad160101@gmail.com>
2020-02-26 17:40:48 +0700

    Add Summ function

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+int summ(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
+    int n = 8;
+    int m = 2;
+
     printf("Hello, World!");
+    printf("%d + %d = %d", n, m, summ(n, m));
+
     return 0;
 }
